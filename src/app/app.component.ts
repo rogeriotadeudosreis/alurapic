062b6b155a007effb07c9c54147527f1f19e7336
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-08-05 14:57:29 -0300

    trazendo as fotos direto da API server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  photos = [];
+  photos: Object[] = [];
 
     constructor(http: HttpClient){
-      console.log(http)
+
+      http
+      .get<Object[]>("http://localhost:3000/flavio/photos")
+      .subscribe(photos => this.photos = photos);
     }
 }
