92d2e574d87723a659712c95480fb51b8b27cef4
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-08-05 09:15:15 -0300

    implementado um data-binding para exemplo de entendimento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alurapic';
+
+  description = 'image of this a home';
+  url =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmsztWCh6tvZGr8HQKHblbIEJvJ8eLE2ki9Q&usqp=CAU';
 }
