f6e2807c9acbd8131d6f8cdc67e91c1f88ddbc11
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-08-05 19:23:07 -0300

    implementado o componente errors.module.ts e errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
