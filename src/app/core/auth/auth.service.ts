7f028f67100b40549bdd13657910e2ebe4c6c5e0
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2022-04-13 16:09:08 -0300

    Criado o serviço TokenService para manipular o token:token.service
    Criado os métodos set, get, remove e hasToken.

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { TokenService } from './../token/token.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
@@ -8,7 +9,7 @@ const API_URL = 'http://localhost:3000';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   authenticate(userName: string, password: string) {
     return this.http
@@ -20,6 +21,7 @@ export class AuthService {
       .pipe(
         tap((res) => {
           const authToken = res.headers.get('x-access-token');
+          this.tokenService.setToken(authToken);
           console.log(`User ${userName} authenticated with token ${authToken}`);
         })
       );
