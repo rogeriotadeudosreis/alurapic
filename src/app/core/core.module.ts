e336a7d057619325e8404d23a7a3b0adda60457b
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2022-04-15 11:54:47 -0300

    Diretiva routerLink
    Configurado a rota para login, sem ter que carregar toda aplicação, fazendo uso da diretiva {routerLink} do angular.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports:[CommonModule],
   exports: [HeaderComponent],
+  imports: [CommonModule, RouterModule],
 })
 export class CoreModule {}
